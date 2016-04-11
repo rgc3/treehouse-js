e593da221a5eb60037fb52d83ac16f37f45e5905
Robert Cunningham <rgc3dev@gmail.com>
2016-04-11 15:14:58 -0400

    first commit

---
+++ rand_computer_guess.js
@@ -0,0 +1,15 @@
+var upper = 10000; // variable to define the upper limit of the  random number
+var randomNumber = getRandomnumber(upper); // number the computer has to guess, generates a number from 1 - 10000
+var guess; // variable to hold the computer's guess
+var attempts = 0; // variable to hold the number of attempts
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) +1;
+}
+
+while ( guess !== randomNumber ) {
+  guess = getRandomNumber( upper );
+  attempts += 1; // keeps track of the number of guesses
+}
+document.write("<p>The random number was: " + randomNumber + "</p>");
+document.write("<p>It took the computer " + attempts + " get it right.</p>");
